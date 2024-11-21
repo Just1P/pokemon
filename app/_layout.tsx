ae1d3ab950e2610ea66a409eafd3b8e6b0934123
Pitra <justin.pitra@gmail.com>
2024-11-21 23:18:31 +0100

    adding tabs

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,13 +1,57 @@
-import { Stack } from "expo-router";
+import { Tabs } from "expo-router";
+import {
+  Ionicons,
+  MaterialCommunityIcons,
+  MaterialIcons,
+} from "@expo/vector-icons";
 
-const Layout = () => {
+export default function AppLayout() {
   return (
-    <Stack>
-      <Stack.Screen name="index" options={{ headerShown: false }} />
-      <Stack.Screen name="pokemons" options={{ title: "Tous les Pokémon" }} />
-      <Stack.Screen name="team" options={["Combat"]} />
-    </Stack>
+    <Tabs>
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Accueil",
+          tabBarIcon: ({ color, focused }) => (
+            <Ionicons
+              name={focused ? "home" : "home-outline"}
+              size={24}
+              color={color}
+            />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="pokemons"
+        options={{
+          title: "Pokémon",
+          tabBarIcon: ({ color, focused }) => (
+            <MaterialCommunityIcons name="pokeball" size={24} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="team"
+        options={{
+          title: "Combat",
+          tabBarIcon: ({ color }) => (
+            <MaterialIcons name="group" size={24} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="types"
+        options={{
+          title: "Types",
+          tabBarIcon: ({ color, focused }) => (
+            <Ionicons
+              name={focused ? "flash" : "flash-outline"}
+              size={24}
+              color={color}
+            />
+          ),
+        }}
+      />
+    </Tabs>
   );
-};
-
-export default Layout;
+}
